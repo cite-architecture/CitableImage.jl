7ce0cfd6c4606a3d2f0e806317a13abfc1239aec
Neel Smith <dnsmith.neel@gmail.com>
2021-01-09 13:30:45 -0500

    Add optional parameters for height and caption

--- src/iiif.jl
+++ src/iiif.jl
@@ -5,29 +5,48 @@ struct IIIFservice
     directoryroot
 end
 
-function url(img::Cite2Urn, service::IIIFservice)
-    service.baseurl * "?IIIF=" * service.directoryroot * "/" * image_directory(img) * "/" * objectcomponent(dropsubref(img)) * ".tif/full/2000,/0/default.jpg"   
-end
 
-function markdownImage(img::Cite2Urn, service::IIIFservice) #, width::Int   
-    if hassubref(img) 
-        "USE ROI"
+"""
+$(SIGNATURES)
+Compose IIIF URL for retrieving an image.
+"""
+function url(img::Cite2Urn, service::IIIFservice, ht::Int=2000)
+    if hassubref(img)
+        pct = pctString(img)
+        service.baseurl * "?IIIF=" * service.directoryroot * "/" * image_directory(img) * "/" * objectcomponent(dropsubref(img)) * ".tif/$(pct)/$(ht),/0/default.jpg"   
     else
-        "![image](" *  service.baseurl * "?IIIF=" * service.directoryroot * "/" * image_directory(img) * "/" * objectcomponent(dropsubref(img)) * ".tif/full/2000,/0/default.jpg)"  
-    #  res1: String = "![image](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/200,/0/default.jpg)"
+        service.baseurl * "?IIIF=" * service.directoryroot * "/" * image_directory(img) * "/" * objectcomponent(dropsubref(img)) * ".tif/full/$(ht),/0/default.jpg"   
     end
 end
 
-function htmlImage(img::Cite2Urn, service::IIIFservice)
-    #res3: String = "<img class=\"citeImage\" src=\"http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/!75,/0/default.jpg\" />"
-    "TBA"
+"""
+$(SIGNATURES)
+Compose markdown link to IIIF URL for retrieving an image.
+"""
+function markdownImage(img::Cite2Urn, service::IIIFservice, ht::Int=2000, caption::AbstractString="image") 
+    if hassubref(img) 
+        pct = pctString(img)
+        "![$(caption)](" *  service.baseurl * "?IIIF=" * service.directoryroot * "/" * image_directory(img) * "/" * objectcomponent(dropsubref(img)) * ".tif/$(pct)/$(ht),/0/default.jpg)"  
+    else
+        "![$(caption)](" *  service.baseurl * "?IIIF=" * service.directoryroot * "/" * image_directory(img) * "/" * objectcomponent(dropsubref(img)) * ".tif/full/$(ht),/0/default.jpg)"  
+    
+    end
 end
+
+
 
 function linkedMarkdownImage(img, service) #, caption="Folio 12 recto of the Venetus A manuscript of the Iliad")
     #res7: String = "[![Folio 12 recto of the Venetus A manuscript of the Iliad](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/150,/0/default.jpg)](http://www.homermultitext.org/ict2/?urn=urn:cite2:hmt:vaimg.2017a:VA012RN_0013)"
     #res9: String = "[![Metrical summary of book 1](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/pct:15.75,9.332,38.79,3.225/150,/0/default.jpg)](http://www.homermultitext.org/ict2/?urn=urn:cite2:hmt:vaimg.2017a:VA012RN_0013@0.1575,0.09332,0.3879,0.03225)"
     "TBA"
 end
+
+
+function htmlImage(img::Cite2Urn, service::IIIFservice)
+    #res3: String = "<img class=\"citeImage\" src=\"http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/!75,/0/default.jpg\" />"
+    "TBA"
+end
+
 
 function linkedHtmlImage(img, service) #, caption="Folio 12 recto of the Venetus A manuscript of the Iliad")
     #  String = "<a href=\"http://www.homermultitext.org/ict2/?urn=urn:cite2:hmt:vaimg.2017a:VA012RN_0013\"><img class=\"citeImage\" src=\"http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/!,150/0/default.jpg\" /></a>"
--- test/test_urls.jl
+++ test/test_urls.jl
@@ -18,4 +18,11 @@ end
     urn = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA012RN_0013")
     expected = "![image](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/2000,/0/default.jpg)"
     @test markdownImage(urn, svc) == expected
+
+    alpha = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA012RN_0013@0.2096,0.2112,0.1617,0.01892")
+    expectedRoi = "![image](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/pct:20.96,21.12,16.17,1.892/2000,/0/default.jpg)"
+    @test markdownImage(alpha, svc) == expectedRoi
+
+    expectedCaption = expectedCaption = "![Iliad 1](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/pct:20.96,21.12,16.17,1.892/250,/0/default.jpg)"
+    @test markdownImage(alpha, svc, 250, "Iliad 1") == expectedCaption
 end
