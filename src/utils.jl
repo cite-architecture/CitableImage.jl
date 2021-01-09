d5bc962632796bf57eb9efc7d857af31c84c7832
Neel Smith <dnsmith.neel@gmail.com>
2021-01-09 13:15:24 -0500

    Test construction of IIIF percent expressions

--- src/utils.jl
+++ src/utils.jl
@@ -14,14 +14,35 @@ end
 
 """
 $(SIGNATURES)
-Given a region-of-interest string, compute percent values.
+Given a region-of-interest string, compute percent values,
+with result rounded to a given number of digits.
 """
-function roiFloats(s::AbstractString)
+function roiFloats(s::AbstractString, digits::Int = 3)
     parts = split(s, ",")
     if (size(parts, 1) != 4)
         throw(ArgumentError("""Invalid region of interest `$(s)`:  expression must have four comma-separated parts."""))
     else 
         floats = map(roi -> parse(Float64, roi) * 100, parts)
-        floats
+        map(num -> round(num, digits=digits), floats)
     end
 end
+
+"""
+$(SIGNATURES)
+Given a region-of-interest string, compose percent expression for IIIF.
+"""
+function pctString(s::AbstractString)
+    floats = join(roiFloats(s), ",")
+    "pct:" * floats
+end
+
+"""
+$(SIGNATURES)
+Given a CITE2 URN with region-of-interest subreference, compose percent expression for IIIF.
+"""
+function pctString(u::Cite2Urn)
+    roi = subref(u)
+    floats = join(roiFloats(roi), ",")
+    "pct:" * floats
+end
+
--- test/test_rois.jl
+++ test/test_rois.jl
@@ -7,3 +7,12 @@
     @test floats[3] ≈ 32.345
     @test floats[4] ≈ 42.345
 end
+
+@testset "Compose IIIF percent expressions" begin
+    roi = "0.12345,0.22345,0.32345,0.42345"
+    expected = "pct:12.345,22.345,32.345,42.345" 
+    @test CitableImage.pctString(roi) == expected
+
+    urn = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA012RN_0013@0.12345,0.22345,0.32345,0.42345")
+    @test CitableImage.pctString(urn) == expected
+end
