e1985405d8cd9c5c9ea33a05d8fe91d087bfe682
Neel Smith <nsmith@Technites.local>
2021-01-09 08:51:33 -0500

    Define required functions

--- src/CitableImage.jl
+++ src/CitableImage.jl
@@ -5,6 +5,9 @@ using CitableBase
 
 export Iiifservice
 export image_directory
+export url, markdownImage, htmlImage, 
+    linkedMarkdownImage, linkedHtmlImage
+
 
 include("iiif.jl")
 include("pathutils.jl")
--- src/iiif.jl
+++ src/iiif.jl
@@ -4,8 +4,26 @@ struct IIIFservice
     directoryroot
 end
 
-
-function url(urn::Cite2Urn, service:::IIIFservice)
-    
-end
+function url(img::Cite2Urn, service:::IIIFservice)
+# res0: String = "http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/2000,/0/default.jpg"
 # s"${baseUrl}IIIF=${imagePath}${imageID}.tif/${roiComponent}/${sizing}/0/default.jpg"
+   
+end
+
+function markdownImage(img::Cite2Urn, service:::IIIFservice) #, width::Int
+   )
+#  res1: String = "![image](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/200,/0/default.jpg)"
+end
+
+function htmlImage(img::Cite2Urn, service:::IIIFservice)
+    #res3: String = "<img class=\"citeImage\" src=\"http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/!75,/0/default.jpg\" />"
+end
+
+function linkedMarkdownImage(img, service) #, caption="Folio 12 recto of the Venetus A manuscript of the Iliad")
+    #res7: String = "[![Folio 12 recto of the Venetus A manuscript of the Iliad](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/150,/0/default.jpg)](http://www.homermultitext.org/ict2/?urn=urn:cite2:hmt:vaimg.2017a:VA012RN_0013)"
+    #res9: String = "[![Metrical summary of book 1](http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/pct:15.75,9.332,38.79,3.225/150,/0/default.jpg)](http://www.homermultitext.org/ict2/?urn=urn:cite2:hmt:vaimg.2017a:VA012RN_0013@0.1575,0.09332,0.3879,0.03225)"
+end
+
+function linkedHtmlImage(img, service) #, caption="Folio 12 recto of the Venetus A manuscript of the Iliad")
+    #  String = "<a href=\"http://www.homermultitext.org/ict2/?urn=urn:cite2:hmt:vaimg.2017a:VA012RN_0013\"><img class=\"citeImage\" src=\"http://www.homermultitext.org/iipsrv?IIIF=/project/homer/pyramidal/deepzoom/hmt/vaimg/2017a/VA012RN_0013.tif/full/!,150/0/default.jpg\" /></a>"
+end
