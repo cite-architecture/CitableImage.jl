70dc64469a7d966a763aac2fe3353710714f8a7b
Neel Smith <dnsmith.neel@gmail.com>
2022-02-01 04:07:15 -0500

    Add citable trait for ImgeRecord

--- src/image.jl
+++ src/image.jl
@@ -22,6 +22,46 @@ function ==(img1::ImageRecord, img2::ImageRecord)
 end
 
 
+
+
+
+
+
+"Define singleton type to use as value of `CitableTrait` on `ImageRecord`."
+struct ImageCitable <: CitableTrait end
+"""Set value of `CitableTrait` for `ImageRecord`.
+$(SIGNATURES)
+"""
+function citabletrait(::Type{ImageRecord})
+    ImageCitable()
+end
+
+"""Type of URN identifying an `ImageRecord`.
+$(SIGNATURES)
+Required function for `Citable` abstraction.
+"""
+function urntype(::Type{ImageRecord})
+    Cite2Urn
+end
+
+
+"""URN identifying `img`.
+$(SIGNATURES)
+Required function for `Citable` abstraction.
+"""
+function urn(img::ImageRecord)
+    img.urn
+end
+
+"""Label for `img`.
+$(SIGNATURES)
+Required function for `Citable` abstraction.
+"""
+function label(img::ImageRecord)
+    img.caption
+end
+
+
 # Example CEX:
 # urn:cite2:hmt:vaimg.2017a:VA083RN_0084|Venetus A: Marcianus Graecus Z. 454 (= 822).  Photograph in natural light, folio 83, recto.|This image was derived from an original ©2007, Biblioteca Nazionale Marciana, Venezia, Italia. The derivative image is ©2010, Center for Hellenic Studies. Original and derivative are licensed under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 License. The CHS/Marciana Imaging Project was directed by David Jacobs of the British Library.
 
--- test/test_image.jl
+++ test/test_image.jl
@@ -1,4 +1,4 @@
-@testset "Test basic imagerecord structure" begin
+@testset "Test basic imagerecord structure and citable trait" begin
     u = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA083RN_0084")
     caption =  "folio 83 recto, natural light"
     lic = "CC-by-share"
@@ -7,4 +7,9 @@
 
     @test string(img) == "<urn:cite2:hmt:vaimg.2017a:VA083RN_0084> folio 83 recto, natural light"
     @test img == img2
+
+    @test citable(img)
+    @test urntype(typeof(img)) == Cite2Urn
+    @test urn(img) == u
+    @test label(img) == caption
 end
