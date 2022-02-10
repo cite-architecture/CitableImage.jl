e6f8519d04f1ee3336b8c853526111ebcdb9d4f2
Neel Smith <dnsmith.neel@gmail.com>
2022-02-10 05:04:34 -0500

    Test RoI types

--- src/tlhwpctrect.jl
+++ src/tlhwpctrect.jl
@@ -14,29 +14,51 @@ struct TLHWpctRectRoi <: AbstractRectRoi
 end
 
 
+"""Minimum Y value in pixels for region of interest.
+$(SIGNATURES)
+"""
 function top(r::TLHWpctRectRoi)
     toppix = trunc(Int,r.t * r.y)
     toppix == 0 ? 1 : toppix    
 end
 
+
+"""Minimum X value in pixels for region of interest.
+$(SIGNATURES)
+"""
 function left(r::TLHWpctRectRoi)
     leftpix = trunc(Int,r.l * r.x)
     leftpix == 0 ? 1 : leftpix
 end
 
+
+"""Height in pixels of region of interest.
+$(SIGNATURES)
+"""
 function h(r::TLHWpctRectRoi)
     trunc(Int,r.h * r.y)
 end
 
+"""Width in pixels of region of interest.
+$(SIGNATURES)
+"""
 function w(r::TLHWpctRectRoi)
     trunc(Int,r.w * r.x)
 end
 
+
+"""Maximim Y value of region of interest.
+$(SIGNATURES)
+"""
 function bottom(r::TLHWpctRectRoi)
     bottompix = trunc(Int,(r.t + r.h) * r.y)
     bottompix == 0 ? 1 : bottompix
 end
 
+
+"""Maximim X value of region of interest.
+$(SIGNATURES)
+"""
 function right(r::TLHWpctRectRoi)
     rightpix = trunc(Int,(r.l + r.w) * r.x)
     rightpix == 0 ? 1 : rightpix
