dd07d3affe9f2dfbb9d1c85b566033ee24bd611c
Neel Smith <dnsmith.neel@gmail.com>
2022-02-10 06:33:46 -0500

    Test local image implementation

--- test/test_files.jl
+++ test/test_files.jl
@@ -5,8 +5,14 @@
     imgurn = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA012RN_0013@0.15,0.10,0.10,0.1")
     @test isfile(CitableImage.imgpath(imgsrc, imgurn, extension = "jpg"))
 
-    flatsrc = LocalImageFiles("data", false)
+    flatsrc = LocalImageFiles(joinpath(pwd(), "data"), false)
     @test flatsrc isa LocalImageFiles
     flatdirimg = Cite2Urn("urn:cite2:citeecod:codsang250imgs.v1:sg250_478_477_0@0.2458,0.3005,0.07813,0.02664")
     @test isfile(CitableImage.imgpath(flatsrc, flatdirimg, extension = "jpg"))
+    data  = imagedata(flatsrc, flatdirimg)
+    @test size(data) == (2000, 459)
+
+    data500  = imagedata(flatsrc, flatdirimg, ht = 500)
+    @test xize(data500) == (500, 114)
+
 end
